4f1cd34767736f2cd0871138ea2ecbd636a503d4
Marion <Marion@Server1>
2020-05-23 14:23:01 +0800

    Added New Pies (Apple, Beetroot, Berry, Carrot and Chorus)

---
+++ src/main/java/com/karogath/enhancedvanilla/item/BeetrootPieItem.java
@@ -0,0 +1,37 @@
+
+package com.karogath.enhancedvanilla.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.UseAction;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+
+import com.karogath.enhancedvanilla.EnhancedvanillaModElements;
+
+@EnhancedvanillaModElements.ModElement.Tag
+public class BeetrootPieItem extends EnhancedvanillaModElements.ModElement {
+	@ObjectHolder("enhancedvanilla:beetroot_pie")
+	public static final Item block = null;
+	public BeetrootPieItem(EnhancedvanillaModElements instance) {
+		super(instance, 307);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new FoodItemCustom());
+	}
+	public static class FoodItemCustom extends Item {
+		public FoodItemCustom() {
+			super(new Item.Properties().group(ItemGroup.FOOD).maxStackSize(64).food((new Food.Builder()).hunger(8).saturation(1.5f).build()));
+			setRegistryName("beetroot_pie");
+		}
+
+		@Override
+		public UseAction getUseAction(ItemStack par1ItemStack) {
+			return UseAction.EAT;
+		}
+	}
+}
---
+++ src/main/java/com/karogath/enhancedvanilla/item/CarrotPieItem.java
@@ -0,0 +1,37 @@
+
+package com.karogath.enhancedvanilla.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.UseAction;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+
+import com.karogath.enhancedvanilla.EnhancedvanillaModElements;
+
+@EnhancedvanillaModElements.ModElement.Tag
+public class CarrotPieItem extends EnhancedvanillaModElements.ModElement {
+	@ObjectHolder("enhancedvanilla:carrot_pie")
+	public static final Item block = null;
+	public CarrotPieItem(EnhancedvanillaModElements instance) {
+		super(instance, 309);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new FoodItemCustom());
+	}
+	public static class FoodItemCustom extends Item {
+		public FoodItemCustom() {
+			super(new Item.Properties().group(ItemGroup.FOOD).maxStackSize(64).food((new Food.Builder()).hunger(8).saturation(1.5f).build()));
+			setRegistryName("carrot_pie");
+		}
+
+		@Override
+		public UseAction getUseAction(ItemStack par1ItemStack) {
+			return UseAction.EAT;
+		}
+	}
+}
